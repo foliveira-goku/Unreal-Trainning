06cac42903b7923ef28167cfcad46ca3f3d050d4
Fernanda Oliveira <tophinha@gmail.com>
2023-08-01 19:33:36 -0300

    Checkpoint system added

--- Source/Trainning_Project/Checkpoint.cpp
+++ Source/Trainning_Project/Checkpoint.cpp
@@ -1,9 +1,22 @@
 
 #include "Checkpoint.h"
+#include "Components/CapsuleComponent.h"
+#include "Particles/ParticleSystemComponent.h"
+//#include "NiagaraFunctionLibrary.h"
+#include "NiagaraComponent.h"
 
 ACheckpoint::ACheckpoint()
 {
  	PrimaryActorTick.bCanEverTick = true;
+
+	triggerComponent = CreateDefaultSubobject<UCapsuleComponent>(TEXT("Trigger"));
+	RootComponent = triggerComponent;
+
+	particleSystem = CreateDefaultSubobject<UNiagaraComponent>(TEXT("Particles"));
+	particleSystem->SetupAttachment(RootComponent);
+	
+	meshComponent = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Mesh"));
+	meshComponent->SetupAttachment(RootComponent);
 }
 
 void ACheckpoint::BeginPlay()
@@ -14,5 +27,13 @@ void ACheckpoint::BeginPlay()
 void ACheckpoint::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
+}
+
+void ACheckpoint::OnCheckpointReached()
+{
+	UE_LOG(LogTemp, Warning, TEXT("CheckPointReached at: %f %f"), GetActorLocation().X, GetActorLocation().Y);
+	particleSystem->Activate();
+	triggerComponent->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+	meshComponent->SetMaterial(0, activatedMaterial);
 }
 
--- Source/Trainning_Project/Checkpoint.h
+++ Source/Trainning_Project/Checkpoint.h
@@ -4,15 +4,31 @@
 #include "GameFramework/Actor.h"
 #include "Checkpoint.generated.h"
 
+class UCapsuleComponent;
+class UNiagaraComponent;
+
 UCLASS()
 class TRAINNING_PROJECT_API ACheckpoint : public AActor
 {
 	GENERATED_BODY()
+
+	UPROPERTY(VisibleAnywhere, Category = "Components")
+	UCapsuleComponent* triggerComponent;
 	
+	UPROPERTY(VisibleAnywhere, Category = "Components")
+	UNiagaraComponent* particleSystem;
 	
+	UPROPERTY(VisibleAnywhere, Category = "Components")
+	UStaticMeshComponent* meshComponent;
+
+	UPROPERTY(EditAnywhere, Category = "Visual")
+	UMaterialInstance* activatedMaterial;
+
 public:
 	ACheckpoint();
 	virtual void Tick(float DeltaTime) override;
+	UFUNCTION(BlueprintCallable)
+	void OnCheckpointReached();
 
 protected:
 	virtual void BeginPlay() override;
